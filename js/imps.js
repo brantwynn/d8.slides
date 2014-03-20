9915753b948746106ca32330a022ab00b3720095
brantwynn <brant.wynn@acquia.com>
2014-03-20 13:59:52 -0500

    added imps!

---
+++ js/imps.js
@@ -0,0 +1,46 @@
+/**
+ * imps.js
+ *
+ * imps.js provides imps (impress.js shortcuts) to help create presentations
+ * without calculating your own math. Combine simple css classes with
+ * impress' .step class to create div data-* attributes on imps.init().
+ *
+ */
+var imps = {
+  w: $(window).width(),
+  h: $(window).height(),
+  init: function() {
+    // TRBL imps
+    $(".step.above").each(function() {
+      a = 'data-y';
+      p = ($(this).prev().attr(a)) ? parseInt($(this).prev().attr(a)) : 0; 
+      $(this).attr(a, p-imps.h);
+      imps.cleanup($(this));
+    });
+    $(".step.ahead").each(function() {
+      a = 'data-x';
+      p = ($(this).prev().attr(a)) ? parseInt($(this).prev().attr(a)) : 0;
+      $(this).attr(a, p+imps.w);
+      imps.cleanup($(this));
+    });
+    $(".step.below").each(function() {
+      a = 'data-y';
+      p = ($(this).prev().attr(a)) ? parseInt($(this).prev().attr(a)) : 0; 
+      $(this).attr(a, p+imps.h);
+      imps.cleanup($(this));
+    });
+    $(".step.behind").each(function() {
+      a = 'data-x';
+      p = ($(this).prev('.step').attr(a)) ? parseInt($(this).prev('.step').attr(a)) : 0;
+      $(this).attr(a, p-imps.w);
+      imps.cleanup($(this));
+    });
+  },
+  cleanup: function($step) {
+    a = ['data-x', 'data-y'];
+    a.forEach(function(e) {
+      n = ($step.attr(e)) ? $step.attr(e) : $step.prev().attr(e);
+      $step.attr(e, parseInt(n));
+    });
+  }
+}
--- js/init.js
+++ js/init.js
@@ -1,13 +1,8 @@
 $(document).ready(function() {
-  // 
-  $(".auto").each(function() {
-    var nextX = parseInt($(this).prev().attr('data-x')) + $(window).width();
-    $(this).attr('data-x', nextX);
-  });
-
   // Set up impress.js
   if ("ontouchstart" in document.documentElement) {
     $(".hint").html("<p>Tap on the left or right to navigate</p>");
   }
+  imps.init();
   impress().init();
 });
